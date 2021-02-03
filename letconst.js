1e1ca1a6ec0cb02db6ad809cb17940847828ecd5
hituhin <hituhin09@gmail.com>
2021-02-03 18:40:24 +0600

    let and const practice

---
+++ letconst.js
@@ -0,0 +1,7 @@
+const hubby = " Elias Kanchon ";
+console.log(hubby);
+const numbers = [12,45];
+numbers[1] = 88 ;
+numbers.push(12);
+numbers.push = ["Sunny", "Salman", "Omit Hasan"];
+console.log(numbers);
